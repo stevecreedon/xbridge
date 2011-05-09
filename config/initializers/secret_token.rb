fbbdbcbb30880d3b4df8c5265f7beeab79416f93
stevecreedon <stephen.creedon@rapidappsgroup.com>
2011-05-09 22:48:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xbridge::Application.config.secret_token = 'a8e36001cf7dda9583477cac43deae05e205ac516b76dd4638ce6679ed3d5abc78acd0b3ccef878cd098feb42eb8b8d3e83d6dcebc2891d745732919ed84b103'
